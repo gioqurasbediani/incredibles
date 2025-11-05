c6f98b3d79543bc8826e66d66452949f0096f86d
gioqurasbediani <46025700+gioqurasbediani@users.noreply.github.com>
2025-11-06 03:52:10 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ document.querySelectorAll('nav a').forEach(a=>{
     document.querySelector(a.getAttribute('href')).scrollIntoView({behavior:'smooth'});
   });
 });
+
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 50){
+    header.classList.add('shrink');
+  } else {
+    header.classList.remove('shrink');
+  }
+});
